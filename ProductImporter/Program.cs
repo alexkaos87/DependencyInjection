c78f0828adb3098b6f3857dc333844c8796d93c2
Alessio Coppola <alessiosalvatore87@gmail.com>
2024-09-16 21:53:40 +0200

    adding DI container instead of manual classes/deps initialization

--- ProductImporter/Program.cs
+++ ProductImporter/Program.cs
@@ -1,14 +1,23 @@
-﻿using ProductImporter.Shared;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ProductImporter.Shared;
 using ProductImporter.Source;
 using ProductImporter.Target;
 
-var configuration = new Configuration();
+using var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        services.AddTransient<Configuration>();
 
-var priceParser = new PriceParser();
-var productSource = new ProductSource(configuration, priceParser);
+        services.AddTransient<IPriceParser, PriceParser>();
+        services.AddTransient<IProductSource, ProductSource>();
 
-var productFormatter = new ProductFormatter();
-var productTarget = new ProductTarget(configuration, productFormatter);
+        services.AddTransient<IProductFormatter, ProductFormatter>();
+        services.AddTransient<IProductTarget, ProductTarget>();
 
-var productImporter = new ProductImporter.ProductImporter(productSource, productTarget);
+        services.AddTransient<ProductImporter.ProductImporter>();
+    })
+    .Build();
+
+var productImporter = host.Services.GetRequiredService<ProductImporter.ProductImporter>();
 productImporter.Run();
