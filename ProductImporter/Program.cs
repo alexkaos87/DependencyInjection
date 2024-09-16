748b75dd50fd55a5900ab976a6aeb2a29a2ecd61
Alessio Coppola <alessiosalvatore87@gmail.com>
2024-09-16 22:08:11 +0200

    introducing import statistics as singleton dependency

--- ProductImporter/Program.cs
+++ ProductImporter/Program.cs
@@ -16,6 +16,8 @@ using var host = Host.CreateDefaultBuilder(args)
         services.AddTransient<IProductTarget, ProductTarget>();
 
         services.AddTransient<ProductImporter.ProductImporter>();
+
+        services.AddSingleton<IImportStatistics, ImportStatistics>();
     })
     .Build();
 
---
+++ ProductImporter/Shared/ImportStatistics.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace ProductImporter.Shared;
+
+public class ImportStatistics : IImportStatistics
+{
+    private int _importedCounter;
+    private int _outputCounter;
+
+    public string GetStatistics()
+    {
+        var buffer = new StringBuilder();
+        buffer.Append($"Read a total of {_importedCounter} products from source");
+        buffer.AppendLine();
+        buffer.Append($"Written a total of {_outputCounter} products to target");
+        
+        return buffer.ToString();
+    }
+
+    public void IncrementImportCount() => ++_importedCounter;
+     
+    public void IncrementOutputCount() => ++_outputCounter;
+}
